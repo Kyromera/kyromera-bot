70c0d469774a5a93091b060b7a2b92a76d6433a1
Fabian Thomys <git@fthomys.me>
2025-06-12 19:19:29 +0200

    updated test command

--- src/main/kotlin/me/diamondforge/kyromera/bot/commands/test.kt
+++ src/main/kotlin/me/diamondforge/kyromera/bot/commands/test.kt
@@ -10,6 +10,7 @@ import io.github.freya022.botcommands.api.commands.application.provider.GlobalAp
 import io.github.freya022.botcommands.api.commands.application.provider.GlobalApplicationCommandProvider
 import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashEvent
 import io.github.freya022.botcommands.api.core.BContext
+import io.github.freya022.botcommands.api.core.utils.send
 import io.github.oshai.kotlinlogging.KotlinLogging
 import me.diamondforge.kyromera.bot.services.LevelService
 import me.diamondforge.kyromera.levelcardlib.wrapper.createLevelCard
@@ -18,6 +19,7 @@ import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 import net.dv8tion.jda.api.utils.FileUpload
 import net.dv8tion.jda.api.utils.TimeFormat
+import java.awt.SystemColor.text
 import java.awt.image.BufferedImage
 import java.io.File
 import java.lang.management.ManagementFactory
@@ -36,23 +38,30 @@ class test(
         event: GuildSlashEvent,
     ) {
         event.deferReply().await()
-        val userexperience = levelService.getExperience(event.user.idLong, event.guild.idLong)
+        val userexperience = levelService.getExperience(event.guild.idLong, event.user.idLong)
         val (min, max) = levelService.MinAndMaxXpForLevel(userexperience.level)
-        val card = event.user.createLevelCard()
+        val card = event.member.createLevelCard()
             .level(userexperience.level)
             .xp(min, max, userexperience.xp)
+            .accentColor(0xFF2CBCC9.toInt())
+            .showStatusIndicator(false)
             .rank(userexperience.rank.toInt())
-            .build().toFile()
-        val fu = FileUpload.fromData(card, "levelcard.png")
-        val msg = MessageCreate { files += fu }
+            .build()
+            .toFile()
+            .toFileUpload()
         
-        event.channel.sendMessage(msg).queue()
+        
+        MessageCreate { files += card }.send(event.hook).queue()
     }
     
     fun BufferedImage.toFile(): File {
         val file = File.createTempFile("image", ".png")
         javax.imageio.ImageIO.write(this, "png", file)
         return file
+    }
+    
+    fun File.toFileUpload(): FileUpload {
+        return FileUpload.fromData(this, this.name)
     }
 
     override fun declareGlobalApplicationCommands(manager: GlobalApplicationCommandManager) {
